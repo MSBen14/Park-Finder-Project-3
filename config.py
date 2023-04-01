f8d427acb047b3caeb43c22d63afff5384a01215
vwalden <walden@plsinfo.org>
2023-04-01 10:18:11 -0700

    Version 3 of working files

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-api_key = "HwheFPglqOJfjazz7qc80cpRl84qJgRJhEdELN1a"
+# OpenWeatherMap API Key
+weather_key = "ff9a06ef19d32546500a6a9dc3355d2e"
+
+# Geoapify API Key
+geoapify_key = "855b57f8621548b9ab66c56d633b6ada"
+
+# NPS API Key
+api_key = "8erM7SeDusOmTbjN2Y5VimKTgMhvadfvOudvESGr"
